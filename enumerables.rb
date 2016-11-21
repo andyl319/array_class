e75cb147360f51a8bf76754f099c2c840e7b65bd
Andrew Lim <admin@Admins-MBP.lan>
2016-11-20 20:44:59 -0800

    Write a my_each method that replicates the Array.each method

---
+++ enumerables.rb
@@ -0,0 +1,110 @@
+require 'byebug'
+
+class Array
+
+  def my_each(&prc)
+    for x in (0..self.length - 1)
+      prc.call(self[x])
+    end
+  end
+
+  def my_select(&prc)
+    arr = []
+    self.my_each do |x|
+      if (prc.call(x))
+        arr << x
+      end
+    end
+    arr
+  end
+
+  def my_reject(&prc)
+    result = []
+
+    self.my_each do |x|
+        if (!prc.call(x))
+          result << x
+        end
+      end
+      result
+    end
+
+
+  def my_any?(&prc)
+    self.my_each do |x|
+      if prc.call(x)
+        return true
+      end
+    end
+    false
+  end
+
+  def my_all?(&prc)
+    self.my_each do |x|
+      if prc.call(x)
+        next
+      else
+        return false
+      end
+    end
+    true
+  end
+
+  def my_flatten
+
+    arr = []
+    self.my_each do |x|
+
+      if(x.class == Array)
+        arr.concat(x.my_flatten)
+      else
+        arr.push(x)
+      end
+
+    end
+    arr
+  end
+
+  def my_zip(*arr)
+    result = []
+    self.each_index do |i|
+      result.push([self[i]])
+      arr.each do |array|
+        result[i].push(array[i])
+      end
+    end
+    result
+  end
+
+  def my_rotate(num = 1)
+    arr = self.dup
+
+    if num > 0
+      num.times { arr.push(arr.shift) }
+    else
+      (num * -1).times { arr.unshift(arr.pop) }
+    end
+    arr
+  end
+
+  def my_join(char="")
+    new_string = ""
+    self.each_with_index do |letter, idx|
+      if idx == self.length - 1
+        new_string << letter
+      else
+        new_string << letter + char
+      end
+    end
+    new_string
+  end
+
+  def my_reverse
+    new_array = []
+    for x in (self.length - 1).downto(0)
+      new_array << self[x]
+    end
+    new_array
+  end
+
+end
